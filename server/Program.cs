0cbaa83b61e58c8c0a5d7b92f462a0bddc8a30e5
Alexios10 <sattar_saud@hotmail.com>
2026-02-26 20:16:55 +0100

    new domain names added

--- server/Program.cs
+++ server/Program.cs
@@ -84,7 +84,8 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("https://application-tracker-five-pi.vercel.app", "http://localhost:8080")
+        policy.WithOrigins("https://application-tracker-five-pi.vercel.app", "http://localhost:8080", "https://minesoknader.no",
+            "https://www.minesoknader.no")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
